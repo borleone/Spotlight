94c1e9757c0c8cf256a153d8e38279834eac5f8d
borleone <pushkar.borle@gmail.com>
2017-04-02 17:50:19 -0400

    Fixed bugs

--- src/GUI.java
+++ src/GUI.java
@@ -6,47 +6,42 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
-import javafx.stage.StageStyle;
 
 /**
  * Created by harsh on 2017-03-25.
  */
 public class GUI {
 
-    public GridPane addGridPane(Pos pos, int HGap, int VGap, Insets padding) {
-        GridPane grid = new GridPane();
-        grid.setAlignment(pos);
-        grid.setHgap(HGap);
-        grid.setVgap(VGap);
-        grid.setPadding(padding);
+	public GridPane addGridPane(Pos pos, int HGap, int VGap, Insets padding) {
+		GridPane grid = new GridPane();
+		grid.setAlignment(pos);
+		grid.setHgap(HGap);
+		grid.setVgap(VGap);
+		grid.setPadding(padding);
 
-        return grid;
-    }
+		return grid;
+	}
 
-    public Text addText(String text, Font font) {
-        Text scenetitle = new Text(text);
-        scenetitle.setFont(font);
+	public Text addText(String text, Font font) {
+		Text scenetitle = new Text(text);
+		scenetitle.setFont(font);
 
-        return scenetitle;
-    }
+		return scenetitle;
+	}
 
-    public Button addButton(String text, Boolean visibility) {
-        Button button = new Button(text);
-        button.setVisible(visibility);
+	public Button addButton(String text, Boolean visibility) {
+		Button button = new Button(text);
+		button.setVisible(visibility);
 
-        return button;
-    }
+		return button;
+	}
 
-    public static Alert makeAlert(AlertType typeOfAlert, String title, String headerText, String contentText) {
-        Alert alert = new Alert(typeOfAlert);
-        alert.setHeaderText(headerText);
-        alert.setTitle(title);
-        alert.setContentText(contentText);
+	public static Alert makeAlert(AlertType typeOfAlert, String title, String headerText, String contentText) {
+		Alert alert = new Alert(typeOfAlert);
+		alert.setHeaderText(headerText);
+		alert.setTitle(title);
+		alert.setContentText(contentText);
 
-        return alert;
-    }
-<<<<<<< HEAD
+		return alert;
+	}
 }
-=======
-}
->>>>>>> origin/F/GUI
